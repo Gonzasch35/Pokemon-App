cd9f4ef7128c9db16a4def5a12761e51bec31a78
Gonzalo Schmidt <gonza.sch35@gmail.com>
2023-11-21 12:07:30 -0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Favorites from './components/Favorites/Favorites'
 import { useState } from 'react'
 import ErrorPage from './Views/ErrorPage/ErrorPage'
 import axios from 'axios'
-axios.defaults.baseURL = 'https://pokemon-app-production-636a.up.railway.app/'
+axios.defaults.baseURL = 'https://ztqnhqwekbyivcbwkwpi.supabase.co/'
 
 function App() {
   
